a940a66b50b6ec90d24bae800ac5a49ce94c21cf
Mochacycle13 <mmmoore13@gmail.com>
2020-04-26 22:49:43 -0400

    played with the ViewBag.Greeting operation. Created and deleted a NewTerm Model. Will revisit shortly to better create the form after clarifying the MVC communication process.

---
+++ StudyApp/V2/StudyAppMark2/StudyAppMark2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using StudyAppMark2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudyAppMark2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Creeting = hour < 12 ? "GoodMorning" : "Good Afternoon";
+            return View("Index");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your Contact Page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+            //return View(new ErrorViewModel
+            //{
+            //    RequestID = Activity.Current?.Id
+            //        ?? HttpContext.TraceIdentifier
+            //});
+        }
+    }
+}
